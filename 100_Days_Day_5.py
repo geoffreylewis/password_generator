1b389c49bbbf9340a888511ac1d60765bcbb7274
Geoff Lewis <34752513+geoffreylewis@users.noreply.github.com>
2024-04-27 13:52:01 -0500

    Add files via upload
    Starting project...

---
+++ 100_Days_Day_5.py
@@ -0,0 +1,14 @@
+######################
+# Password Generator #
+######################
+
+# Beginning
+print("Time to get you a new password that won't be cracked in 2 seconds!\n")
+
+# Clarifying Questions
+# "How many letters would you like in your password?"
+# "How many symbols would you like?"
+# "How many numbers would you like?"
+
+# Generate Password
+# "Here is your password: "
